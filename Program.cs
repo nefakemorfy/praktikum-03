d97714ad3628c4e6f570860e4d374650a457c833
nefakemorfy <125242983+nefakemorfy@users.noreply.github.com>
2023-03-19 22:28:17 +0200

    задача 1. часть 2

--- Program.cs
+++ Program.cs
@@ -13,15 +13,18 @@ namespace ConsoleApp2
             // найти максимальный элемент и его индекс.
             int[] array = { 8, -1, 4, 16, -5, 3 };
             int max = array[0];
+            int indexMax = 0;
             for (int i = 1; i < array.Length; i++)
             {
                 if (array[i] > max)
                 {
                     max = array[i];
+                    indexMax = i;
                 }
             }
-            Console.WriteLine($"max={max}");
-            Console.ReadLine();
+
+            Console.WriteLine($"max={max}\tindexMax={indexMax}");
+            Console.Read();
         }
     }
 }
