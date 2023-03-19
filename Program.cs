cd362e90e21cc3042c171381581c52dc76fcce7f
nefakemorfy <125242983+nefakemorfy@users.noreply.github.com>
2023-03-19 22:16:29 +0200

    задача 1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            // найти максимальный элемент и его индекс.
+            int[] array = { 8, -1, 4, 16, -5, 3 };
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine($"max={max}");
+            Console.ReadLine();
         }
     }
 }
